e2679b14a353724f02929e5621d00065faffbbe5
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-03-02 22:53:45 -0500

    Added Stylings, Changed Picture, Added Main Section and Actions
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -9,34 +9,92 @@ const Landing = ({ setSelectedPage }) => {
   return (
     <section
       id='home'
-      className='md: flex md:justify-between md: items-center md:h-full gap-16 py-10'
+      className='md:flex md:justify-between md: items-center md:h-full gap-16 py-10'
     >
       {/* IMAGE SECTION */}
-      <div className='md:order-2 flex justify-center basis-5/5 z-10 mt-16 md:mt-32'>
+      <div className='md:order-2 flex justify-center basis-1/2 z-10 mt-16 md:mt-32'>
         {isAboveMediumScreens ? (
           <div
-            className='relative z-0 ml-20 before:absolute before:-top-20 
-            before:-left-20 before:rounded-t-[400px] before:w-full 
-            before:max-w-[400px] before:h-full  before:border-2
-            before:border-blue before:z-[-1]'
+            className='relative z-0 ml-20 before:absolute before:-top-[60px] 
+              before:-left-[60px]  before:w-full 
+              before:max-w-[800px] before:h-full  before:border-2
+             before:border-blue before:z-[-1]'
           >
             <img
-              src='assets/profile-image.png'
+              src='assets/Keeshigan-Temp.jpg'
               alt='profile'
               className='hover:filter hover:saturate-150 transition duration-500 
-              z-10 w-full max-w-[400px] md:max-w-[600px]'
+                z-10 w-full max-w-[400px] md:max-w-[600px]'
             />
           </div>
         ) : (
           <img
-            src='assets/profile-image.png'
+            src='assets/Keeshigan-Temp.jpg'
             alt='profile'
             className='hover:filter hover:saturate-150 transition duration-500 
               z-10 w-full max-w-[400px] md:max-w-[600px]'
           />
         )}
-        {/* MAIN SECTION */}
-        <div className='z-30 basis-2/5 mt-12 md:mt-32'></div>
+      </div>
+      {/* MAIN SECTION */}
+      <div className='z-30 basis-1/2 mt-12 md:mt-32'>
+        {/* HEADINGS */}
+        <motion.div
+          initial='hidden'
+          whileInView='visible'
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className='text-4xl font-playfair z-10 text-center md:text-start'>
+            Keeshigan {''}
+            <span
+              className='xs:relative xs:text-deep-blue xs:font-semibold z-20 
+                bg-gradient-orange before:absolute before:-left-[25px] 
+                before:-top-[90px] before:z-[-1]'
+            >
+              Pirabaharan
+            </span>
+          </p>
+          <p className='mt-10 mb-7 text-sm text-center md:text-start'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          </p>
+        </motion.div>
+
+        {/* CALL TO ACTIONS */}
+
+        <motion.div
+          className='flex mt-5 justify-center md:justify-start'
+          initial='hidden'
+          whileInView='visible'
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <AnchorLink
+            className='bg-gradient-orange text-deep-blue rounded-sm py-3 px-7
+             font-semibold hover:bg-blue hover:text-white transition duration-500'
+            onClick={() => setSelectedPage('contact')}
+            href='#contact'
+          >
+            Contact Me
+          </AnchorLink>
+          <AnchorLink className='rounded-r-sm bg-gradient-orange text-white py-0.5 pr-0.5'>
+            <div
+              className='bg-deep-blue hover:text-yellow transition duration-500
+             w-full h-full flex items-center justify-center font-playfair px-10'
+            >
+              Lets' talk.
+            </div>
+          </AnchorLink>
+        </motion.div>
       </div>
     </section>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,8 @@ module.exports = {
       colors: {
         'deep-blue': '#010026',
         blue: '#2CBCE9',
-        red: '#800000',
+        red: '#781423',
+        'bright-red': '#ab5261',
         yellow: '#FDCC49',
         grey: '#ededed',
         'dark-grey': '#757575',
@@ -16,9 +17,12 @@ module.exports = {
       backgroundImage: (theme) => ({
         'gradient-rainbow':
           'linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)',
-
         'gradient-rainblue':
           'linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)',
+        'gradient-predawn': 'linear-gradient(90deg, #FFA17F, #00223E)',
+        'gradient-shore': 'linear-gradient(90deg, #70e1f5, #ffd194)',
+        'gradient-starfall': 'linear-gradient(90deg, #f0c27b, #4b1248)',
+        'gradient-orange': 'linear-gradient(90deg, #EA5A6F, #DE791E, #FCCF3A)',
       }),
       fontFamily: {
         playfair: ['Playfair Display', 'serif'],
