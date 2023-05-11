8a15e3b984f2b77e50b9d06c7ad36bbff1e367c5
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-05-10 23:48:26 -0400

    Added Contact Info
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- 3D-Portfolio/src/components/Contact.jsx
+++ 3D-Portfolio/src/components/Contact.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { contactInfo } from '../constants';
+import { EarthCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { slideIn, fadeIn } from '../utils';
+
+const InfoCard = ({ index, name, value }) => {
+  return (
+    <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
+      <div className='flex flex-row md:flex-col items-baseline'>
+        <p className='font-bold text-2xl w-[150px]'>{name}</p>
+        <p className='pl-8 md:pl-0 md:pt-4 font-normal text-xl'>{value}</p>
+      </div>
+    </motion.div>
+  );
+};
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className={`flex flex-col-reverse gap-10 overflow-hidden`}>
+      <motion.div
+        variants={slideIn('left', 'tween', 0.2, 1)}
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
+        <p className={styles.sectionSubText}>Don't Be A Stranger</p>
+        <h3 className={styles.sectionHeadText}>Contact Me At ...</h3>
+        <div className='mt-16 flex flex-col md:flex-row justify-around gap-7'>
+          {contactInfo.map((info, ind) => (
+            <InfoCard key={`project-${ind}`} index={ind} {...info} />
+          ))}
+        </div>
+      </motion.div>
 
-export default Contact
+      <motion.div
+        variants={slideIn('right', 'tween', 0.2, 1)}
+        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+      >
+        <EarthCanvas />
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Contact, 'contact');
--- 3D-Portfolio/src/constants/index.js
+++ 3D-Portfolio/src/constants/index.js
@@ -215,33 +215,6 @@ const experiences = [
   },
 ];
 
-// const testimonials = [
-//   {
-//     testimonial:
-//       'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
-//     name: 'Sara Lee',
-//     designation: 'CFO',
-//     company: 'Acme Co',
-//     image: 'https://randomuser.me/api/portraits/women/4.jpg',
-//   },
-//   {
-//     testimonial:
-//       "I've never met a web developer who truly cares about their clients' success like Rick does.",
-//     name: 'Chris Brown',
-//     designation: 'COO',
-//     company: 'DEF Corp',
-//     image: 'https://randomuser.me/api/portraits/men/5.jpg',
-//   },
-//   {
-//     testimonial:
-//       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-//     name: 'Lisa Wang',
-//     designation: 'CTO',
-//     company: '456 Enterprises',
-//     image: 'https://randomuser.me/api/portraits/women/6.jpg',
-//   },
-// ];
-
 const projects = [
   {
     name: 'Velocity Voyage',
@@ -323,6 +296,21 @@ const projects = [
   },
 ];
 
+const contactInfo = [
+  {
+    name: 'Email',
+    value: 'kpirabaharan3@gmail.com',
+  },
+  {
+    name: 'Phone',
+    value: '+1 416 617 3498',
+  },
+  {
+    name: 'Location',
+    value: 'Toronto, ON',
+  },
+];
+
 export {
   about,
   services,
@@ -334,4 +322,5 @@ export {
   otherTech,
   experiences,
   projects,
+  contactInfo,
 };
