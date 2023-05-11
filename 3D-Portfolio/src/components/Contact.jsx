f6459f8bfa9d3b4bfaf1c0a732b9589c29674c4f
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-05-11 00:33:51 -0400

    Modified Transition
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- 3D-Portfolio/src/components/Contact.jsx
+++ 3D-Portfolio/src/components/Contact.jsx
@@ -9,7 +9,7 @@ import { slideIn, fadeIn } from '../utils';
 
 const InfoCard = ({ index, name, value, href }) => {
   return (
-    <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)}>
+    <motion.div variants={fadeIn('up', 'spring', 1 + index * 0.5, 1, 0.75)}>
       <div className='flex flex-row md:flex-col items-baseline'>
         <p className='font-bold text-2xl w-[150px]'>{name}</p>
         <a className='pl-8 md:pl-0 md:pt-4 font-normal text-xl' href={href}>
@@ -24,12 +24,12 @@ const Contact = () => {
   return (
     <div className={`flex flex-col-reverse gap-10 overflow-hidden`}>
       <motion.div
-        variants={slideIn('left', 'tween', 0.2, 1)}
+        variants={slideIn('up', 'tween', 0.2, 1)}
         className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
       >
         <p className={styles.sectionSubText}>Don't Be A Stranger</p>
         <h3 className={styles.sectionHeadText}>Contact Me At ...</h3>
-        <div className='mt-16 flex flex-col md:flex-row justify-around gap-7'>
+        <div className='mt-16 md:mt-8 flex flex-col md:flex-row justify-around gap-7'>
           {contactInfo.map((info, ind) => (
             <InfoCard key={`project-${ind}`} index={ind} {...info} />
           ))}
@@ -37,7 +37,7 @@ const Contact = () => {
       </motion.div>
 
       <motion.div
-        variants={slideIn('right', 'tween', 0.2, 1)}
+        variants={slideIn('down', 'tween', 0.2, 1)}
         className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
       >
         <EarthCanvas />
