b78a778b1d0173c8ffde0dc6ea121296d70fdae0
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-05-15 12:09:15 -0400

    Removed Planet Files
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- 3D-Portfolio/src/components/Contact.jsx
+++ 3D-Portfolio/src/components/Contact.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { contactInfo } from '../constants';
-import { EarthCanvas } from './canvas';
+
 import { SectionWrapper } from '../hoc';
 import { slideIn, fadeIn } from '../utils';
 
@@ -44,13 +44,6 @@ const Contact = () => {
           ))}
         </div>
       </motion.div>
-
-      {/* <motion.div
-        variants={slideIn('down', 'tween', 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
-      >
-        <EarthCanvas />
-      </motion.div> */}
     </div>
   );
 };
