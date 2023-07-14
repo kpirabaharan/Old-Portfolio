16d1ec3624e534f82e1905a9cf0cef5ed508e6d0
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-14 17:06:03 -0400

    Actually Fixed Projects Div Now
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,9 +17,59 @@ const ProjectCard = ({
   return (
     <motion.div
       variants={fadeIn('up', 'spring', index * 0.5, 0.75)}
-      className='w-full'
+      className='relative flex bg-dark-teal rounded-2xl w-full sm:w-[360px] h-[350px]'
     >
-      <div className='relative flex bg-dark-teal rounded-2xl w-full sm:w-[360px] h-[350px]'>
+      <div className='absolute inset-0 flex justify-between m-6 z-10'>
+        <div>
+          {website_link ? (
+            <a>
+              <div
+                onClick={() => {
+                  window.open(website_link, '_blank');
+                }}
+                className='black-gradient w-10 h-10 rounded-full flex justify-center 
+                    items-center cursor-pointer'
+              >
+                <img
+                  src={web}
+                  alt='website'
+                  className='w-1/2 h-1/2 object-contain z-20'
+                />
+              </div>
+            </a>
+          ) : (
+            <div></div>
+          )}
+        </div>
+        <div>
+          {source_code_link ? (
+            <a>
+              <div
+                onClick={() => {
+                  window.open(source_code_link, '_blank');
+                }}
+                className='black-gradient w-10 h-10 rounded-full flex justify-center 
+                    items-center cursor-pointer'
+              >
+                <img
+                  src={github}
+                  alt='github'
+                  className='w-1/2 h-1/2 object-contain z-20'
+                />
+              </div>
+            </a>
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+      <div
+        className='absolute h-full w-full rounded-2xl opacity-0 hover:opacity-90 
+          transition duration-500 flex flex-col justify-center items-center 
+          text-center bg-slate-700 z-10'
+      >
+        <p className='text-2xl text-white'>{name}</p>
+        <p className='mt-7 px-8'>{description}</p>
         <div className='absolute inset-0 flex justify-between m-6 z-10'>
           <div>
             {website_link ? (
@@ -64,65 +114,13 @@ const ProjectCard = ({
             )}
           </div>
         </div>
-        <div
-          className='absolute h-full w-full rounded-2xl opacity-0 hover:opacity-90 
-          transition duration-500 flex flex-col justify-center items-center 
-          text-center bg-slate-700 z-10'
-        >
-          <p className='text-2xl text-white'>{name}</p>
-          <p className='mt-7 px-8'>{description}</p>
-          <div className='absolute inset-0 flex justify-between m-6 z-10'>
-            <div>
-              {website_link ? (
-                <a>
-                  <div
-                    onClick={() => {
-                      window.open(website_link, '_blank');
-                    }}
-                    className='black-gradient w-10 h-10 rounded-full flex justify-center 
-                    items-center cursor-pointer'
-                  >
-                    <img
-                      src={web}
-                      alt='website'
-                      className='w-1/2 h-1/2 object-contain z-20'
-                    />
-                  </div>
-                </a>
-              ) : (
-                <div></div>
-              )}
-            </div>
-            <div>
-              {source_code_link ? (
-                <a>
-                  <div
-                    onClick={() => {
-                      window.open(source_code_link, '_blank');
-                    }}
-                    className='black-gradient w-10 h-10 rounded-full flex justify-center 
-                    items-center cursor-pointer'
-                  >
-                    <img
-                      src={github}
-                      alt='github'
-                      className='w-1/2 h-1/2 object-contain z-20'
-                    />
-                  </div>
-                </a>
-              ) : (
-                <div></div>
-              )}
-            </div>
-          </div>
-        </div>
-        <div className='absolute h-full w-full p-4'>
-          <img
-            className='object-cover h-full w-full rounded-2xl'
-            src={image}
-            alt={name}
-          />
-        </div>
+      </div>
+      <div className='absolute h-full w-full p-4'>
+        <img
+          className='object-cover h-full w-full rounded-2xl'
+          src={image}
+          alt={name}
+        />
       </div>
     </motion.div>
   );
@@ -147,7 +145,10 @@ const Projects = () => {
           scalable, and user-friendly software solutions.
         </motion.p>
       </div>
-      <div className='mt-20 justify-center sm:justify-start flex flex-wrap gap-7'>
+      <div
+        className='mt-20 justify-center sm:justify-start flex flex-wrap gap-7 
+        w-full'
+      >
         {projects.map((proj, ind) => (
           <ProjectCard key={`project-${ind}`} index={ind} {...proj} />
         ))}
